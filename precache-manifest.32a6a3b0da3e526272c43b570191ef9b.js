12c029e7f3923103a6e2b87352b3dab553d15b04
davedeveloper <ddave2001@gmail.com>
2020-10-07 21:52:35 +0530

    Updates

---
+++ precache-manifest.32a6a3b0da3e526272c43b570191ef9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a027fb8760e11a279f600f6142c06585",
+    "url": "/club_dashboard/index.html"
+  },
+  {
+    "revision": "f5c891f60d6f2f76fbd0",
+    "url": "/club_dashboard/static/css/main.9b554c93.chunk.css"
+  },
+  {
+    "revision": "e817b0ab3182668b2a58",
+    "url": "/club_dashboard/static/js/2.54a407a3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/club_dashboard/static/js/2.54a407a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c891f60d6f2f76fbd0",
+    "url": "/club_dashboard/static/js/main.c0f2c8a9.chunk.js"
+  },
+  {
+    "revision": "e0df84d47d42012d7ca3",
+    "url": "/club_dashboard/static/js/runtime-main.0bf8c916.js"
+  }
+]);
